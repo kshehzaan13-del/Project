0f4b6780a549eaecd970b11baafa762dc5e3df32
kshehzaan13-del <kshehzaan13@gmail.com>
2025-11-26 15:29:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  server: {
-    host: true,  // allow external network access
-    allowedHosts: [
-      'localhost',
-      '127.0.0.1',
-      'marylouise-pally-zoe.ngrok-free.dev' // <--- add your ngrok host here
-    ]
-  }
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 });
